0f2c94a99616b22d40a7f03e19d14adda6a8f85e
itchenliang <itchenliang@163.com>
2023-03-14 10:17:31 +0800

    feat:切换github源码地址

--- client/public/global.js
+++ client/public/global.js
@@ -2,7 +2,7 @@
 window.g = {
   // 124.222.54.192
   ip: 'http://124.222.54.192:3000',
-  github: 'https://github.com/it-chenliang/info-manage',
+  github: 'https://github.com/ischenliang/info-manage',
   gitee: 'https://gitee.com/itchenliang/info-manage',
   doc: 'https://gitee.com/itchenliang/info-manage/doc'
 }
